ed1796827aba50618ef9c2fe13abf0c1a77d4456
Anna Zadiraka <annazadiraka@gmail.com>
2024-06-23 19:44:27 +0300

    add personal page

---
+++ js/personal.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Скрытие бокового меню
+    const $buttonClose = document.getElementById('close'),
+        $aside = document.getElementById('aside');
+
+        $buttonClose.addEventListener('click', () => {
+            $aside.classList.toggle('show');
+            $buttonClose.classList.toggle('show')
+        })
+
+    // Скрытие подменю
+    const $buttonBoxClose = document.getElementById('closeBox'),
+        $menuAll = document.querySelectorAll('.aside-personal__menu');
+
+        $buttonBoxClose.addEventListener('click', (event) => {
+            
+            $buttonBoxClose.classList.toggle('visible')
+
+            $menuAll.forEach(elem => {    
+                if (elem.dataset.menu === event.target.dataset.menu)
+                elem.classList.toggle('visible');
+            })
+        })
+})
