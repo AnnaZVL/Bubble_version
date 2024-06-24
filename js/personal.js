5f9e26b20d93c7fd75532949dcb63c3bcaea7ae8
Anna Zadiraka <annazadiraka@gmail.com>
2024-06-24 16:36:01 +0300

    add track

--- js/personal.js
+++ js/personal.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const $buttonClose = document.getElementById('close'),
         $aside = document.getElementById('aside');
 
-        $buttonClose.addEventListener('click', () => {
-            $aside.classList.toggle('hidden');
-            $buttonClose.classList.toggle('show')
-        })
+    $buttonClose.addEventListener('click', () => {
+        $aside.classList.toggle('hidden');
+        $buttonClose.classList.toggle('show')
+    })
 
     // Скрытие подменю
     const $buttonBoxClose = document.getElementById('closeBox'),
@@ -20,5 +20,30 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (elem.dataset.menu === event.target.dataset.menu)
                 elem.classList.toggle('visible');
             })
+    })
+
+    //Смена активного пункта в боковой панели
+    const asideMenu = document.querySelector('.aside-menu');
+
+    asideMenu.addEventListener('click', (event) => {
+        const target = event.target;
+        const asideMenuItem = target.closest('.aside-menu__item');
+
+        if (asideMenuItem) {
+            
+            document.querySelectorAll('.aside-menu__item').forEach(item => item.classList.remove('active'));
+
+            asideMenuItem.classList.add('active');
+        }
+    });
+
+    // Смена лайка
+    const $btnLikeAll = document.querySelectorAll('.track__like')
+
+    $btnLikeAll.forEach(like => {
+        like.addEventListener('click', (event) => {
+            console.log('object');
+            like.classList.toggle('like')
         })
+    })
 })
